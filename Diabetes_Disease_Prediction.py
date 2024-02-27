9a62b5618a2e917fe6b7e225c17185f6d8bbf32e
Cvr421 <chandravijayk42187@gmail.com>
2024-02-27 14:18:49 +0530

    Implementing Diabetes prediction algorithm and reading data

--- Diabetes_Disease_Prediction.py
+++ Diabetes_Disease_Prediction.py
@@ -2,9 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,confusion_matrix
 
 dataframe=pd.read_csv("diabetes.csv")
 dataframe.head(10)
 dataframe.info()
+dataframe.describe()
+dataframe.isnull().sum()
+
