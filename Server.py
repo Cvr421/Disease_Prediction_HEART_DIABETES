2a3faef7124ff83c1e312dfca7f1b7b629e27fcd
Cvr421 <chandravijayk42187@gmail.com>
2024-02-28 16:06:19 +0530

    Creating a server for api call

---
+++ Server.py
@@ -0,0 +1,20 @@
+import numpy as np 
+import pandas as pd 
+from flask import Flask, jsonify
+from flask_cors import CORS 
+
+from Diabetes_Disease_Prediction import diabetes_prediction
+from Heart_Disease_prediction import submit_form
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/submit_form',method=['POST'])
+def predict_heart():
+    return submit_form()
+
+@app.route('/submit_form_diabetes', method=['POST'])
+def pre_diabetes():
+    return diabetes_prediction()
+
+if __name__ == '__main__':
+    app.run(debug=True)
