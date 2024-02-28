a0bf5a10dc000a11222153adfd0e497c66d7ddbb
Cvr421 <chandravijayk42187@gmail.com>
2024-02-28 21:57:18 +0530

    Heart user data form layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 import Home from "./Home"
 import { Patient_Diabetes_data } from './Patient_Diabetes_data';
+import { Patient_Heart_data } from './Patient_Heart_data';
 
 function App() {
   return (
@@ -12,7 +13,7 @@ function App() {
 
           <Route exact path="/" element={<Home/>} />
           <Route path="/patient_Diabetes_data" element={<Patient_Diabetes_data />} />
-
+          <Route path="/patient_Heart_data" element={<Patient_Heart_data />} />
         </Routes>
 
       </Router>
---
+++ frontend/src/Patient_Heart_data.js
@@ -0,0 +1,296 @@
+import React from 'react';
+import { useState } from 'react'
+import './Patient_data.css'
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+
+
+export function Patient_Heart_data() {
+  const [formData, setFormData] = useState({
+    age: '',
+    sex: '',
+    chestPain: '',
+    trtbps: '',
+    cholesterol: '',
+    fbs: '',
+    restECG: '',
+    thalachh: '',
+    exng: '',
+    oldPeak: '',
+    slp: '',
+    caa: '',
+    thallium: ''
+  });
+ 
+
+ 
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value
+    }));
+  };
+
+
+  const handleReset = () => {
+    setFormData({
+      age: '',
+      sex: '',
+      chestPain: '',
+      trtbps: '',
+      cholesterol: '',
+      fbs: '',
+      restECG: '',
+      thalachh: '',
+      exng: '',
+      oldPeak: '',
+      slp: '',
+      caa: '',
+      thallium: ''
+    });
+
+  }
+
+
+
+  return (
+    <>
+      <div className='data'>
+        <div className='formdata'>
+          <Form className="patientdata" >
+            <div className="row">
+              <Link className="link" to="/">Home</Link>
+              <h5>HEART REPORT DETAIL</h5>
+              <Form.Text className="text-muted">
+                This is not for clinical Purpose / Please enter only number
+              </Form.Text>
+              <div className="col-sm">
+                <Form.Group controlId="age">
+                  <Form.Label>Age</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Age"
+                    name='age'
+                    value={formData.age}
+                    onChange={handleChange}
+                  />
+
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="sex">
+                  <Form.Label>Sex</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder=" Sex "
+                    name='sex'
+                    value={formData.sex}
+                    onChange={handleChange}
+
+                  />
+                  <Form.Text className="text-muted">
+                    Male=1 / Female=0
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="chestPain">
+                  <Form.Label>Chest Pain</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Between 0 to 3"
+                    name='chestPain'
+                    value={formData.chestPain}
+                    onChange={handleChange}
+
+                  />
+                  <Form.Text className="text-muted">
+                    Chest Pain
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="trtbps">
+                  <Form.Label>TRTBPS</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder=" In (mm Hg)"
+                    name='trtbps'
+                    value={formData.trtbps}
+                    onChange={handleChange}
+
+                  />
+                  <Form.Text className="text-muted">
+                    Resting Blood Pressure
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="cholesterol">
+                  <Form.Label>Cholesterol</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="(in mg/dL)"
+                    name='cholesterol'
+                    value={formData.cholesterol}
+                    onChange={handleChange}
+
+                  />
+
+                  <Form.Text className="text-muted">
+                    cholesterol level
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="fbs">
+                  <Form.Label>FBS.</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Between 0 to 1"
+                    name='fbs'
+                    value={formData.fbs}
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Fasting blood sugar level (in mg/dL)/
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="restECG">
+                  <Form.Label>Rest ECG</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Between 0 to 1"
+                    name='restECG'
+                    value={formData.restECG}
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Resting ECG results
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="thalachh">
+                  <Form.Label>Thalachh</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="THALACHH"
+                    name='thalachh'
+                    value={formData.thalachh}
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Maximum heart rate achieved during exercise
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="exng">
+                  <Form.Label>EXNG</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Betweem 0 to 1"
+                    name='exng'
+                    value={formData.exng}
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Exercise-induced angina
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="oldpeak">
+                  <Form.Label>OldPeak</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Between 0 to 10"
+                    name='oldPeak'
+                    value={formData.oldPeak}
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Abnormality in ECG
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="slp">
+                  <Form.Label>SLP</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Between 0 to 2"
+                    name='slp'
+                    value={formData.slp}
+                    onChange={handleChange} />
+                  <Form.Text className="text-muted">
+                    Indicate the severity of Ischemia
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="caa">
+                  <Form.Label>CAA</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Betweem 0 to 3"
+                    name='caa'
+                    value={formData.caa}
+                    onChange={handleChange}
+                  />
+                  <Form.Text className="text-muted">
+                    Coronary artery disease.
+                  </Form.Text>
+                </Form.Group>
+              </div>
+              <div className="col-sm-2">
+                <Form.Group controlId="thallium">
+                  <Form.Label>Thallium</Form.Label>
+                  <Form.Control
+                    required={true}
+                    type="text"
+                    placeholder="Between 0 to 3"
+                    name='thallium'
+                    value={formData.thallium}
+                    onChange={handleChange}
+
+                  />
+                  <Form.Text className="text-muted">
+                    Thallium stress test
+                  </Form.Text>
+                </Form.Group>
+              </div>
+            </div>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            <Button onClick={handleReset}>
+              Reset
+            </Button>
+          </Form>
+
+        </div>
+
+      </div>
+      
+    </>
+  );
+}
